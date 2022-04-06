341e7954a123aa607aacc7d7f7060a5247de9775
Nikita <80746832+Nikita-Programmer@users.noreply.github.com>
2022-04-06 21:38:56 +0300

    Add files via upload

---
+++ Lab4/Generator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    class Generator
+    {
+        private static int _numbersCount;
+        private static Random _random = new Random();
+
+        internal Generator(int numbersCount)
+        {
+            _numbersCount = numbersCount;
+        }
+
+        // Лучший случай, первые элементы не являются прогрессией
+        internal int[] BestFilling()
+        {
+            int[] array = new int[_numbersCount];
+            array[0] = 2;
+            array[1] = 1;
+            array[2] = 0;
+
+            return array;
+        }
+
+        // Худший случай, в массиве прогрессия
+        internal int[] WorstFilling()
+        {
+            int[] array = new int[_numbersCount];
+
+            for (int i = 0; i < _numbersCount; i++)
+            {
+                array[i] = i + 2;
+            }
+
+            return array;
+        }
+
+        // Средний случай, половина элементов - прогрессия, половина - рандомные
+        internal int[] AverageFilling()
+        {
+            int[] array = new int[_numbersCount];
+
+            for (int i = 0; i < _numbersCount / 2; i++)
+            {
+                array[i] = i + 2;
+            }
+
+            for (int i = _numbersCount / 2; i < _numbersCount; i++)
+            {
+                array[i] = _random.Next(0, _numbersCount);
+            }
+
+            return array;
+        }
+    }
+}
---
+++ Lab4/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Lab4
+{
+    static class Program
+    {
+        //Секундомер
+        static double Stopwatch(int[] array, out bool result)
+        {
+            double timeWork = 0;
+            Stopwatch sw = new Stopwatch();
+            sw.Reset();
+            sw.Start();
+            result = Progression(array);
+            sw.Stop();
+            timeWork += sw.ElapsedTicks;
+
+            return timeWork;
+        }
+
+        //Проверка на прогрессию
+        static bool Progression(int[] mas)
+        {
+            for (int i = 2; i < mas.Length; i++)
+            {
+                if ((mas[i] - mas[i - 1]) != (mas[i - 1] - mas[i - 2]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            List<int> numbersCountList = new List<int> { 10000, 20000, 30000, 40000, 50000 };
+
+            Generator generator = new Generator(10000); // Холостой прогон
+            generator.WorstFilling();
+
+            foreach (int numbersCount in numbersCountList)
+            {
+                generator = new Generator(numbersCount);
+                double timeWorkBest = 0;
+                double timeWorkAverage = 0;
+                double timeWorkWorst = 0;
+                int[] array;
+                bool result;
+
+                for (int i = 1; i <= 5; i++) // 5 запусков
+                {
+                    array = generator.BestFilling(); // Лучший случай
+                    timeWorkBest = Stopwatch(array, out result);
+
+                    array = generator.AverageFilling(); // Средний случай
+                    timeWorkAverage = Stopwatch(array, out result);
+
+                    array = generator.WorstFilling(); // Худший случай
+                    timeWorkWorst = Stopwatch(array, out result);
+                }
+
+                Console.WriteLine("Количество элементов массива - {0}", numbersCount);
+
+                Console.WriteLine("Лучший случай: {0}", timeWorkBest / 5);
+                Console.WriteLine("Средний случай: {0}", timeWorkAverage / 5);
+                Console.WriteLine("Худший случай: {0}", timeWorkWorst / 5);
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
