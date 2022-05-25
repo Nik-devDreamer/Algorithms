79847ca38b984457e34773def349efeaf45550fd
Nikita <nikita.malkin.02@mail.ru>
2022-05-25 19:08:28 +0300

    Lab 9

---
+++ Lab 9/Lab 9 Guard (2.4)/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab_9_Guard__2._4_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество сторожей N >= 2 \nN = ");
+            int N = Int16.Parse(Console.ReadLine());
+
+            Dictionary<double, double> Guard = new Dictionary<double, double>(N);
+            Dictionary<double, double> Protected = new Dictionary<double, double>(N);
+            Console.WriteLine("\nВведите действительные числа [a b] (через пробел) - время прихода и время ухода сторожа (0 <= a,b <= 24)");
+            for (int i = 1; i <= N; i++)
+            {
+                string temp = Console.ReadLine();
+                Guard.Add(double.Parse(temp.Split(" ")[0]), double.Parse(temp.Split(" ")[1]));
+            }
+            Guard.Add(24, 24);
+            Guard = Guard.OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
+
+            double gone = 0;
+            foreach (KeyValuePair<double, double> keyValue in Guard)
+            {
+                if(keyValue.Key - gone > 0 && Guard.Count != 1)
+                {
+                    Protected.Add(gone, keyValue.Key);
+                    gone = keyValue.Value;
+                    Guard.Remove(keyValue.Key);
+                }
+                else if(Guard.Count == 1 && 24 - gone > 0)
+                {
+                    Protected.Add(gone, 24);
+                }
+                else
+                {
+                    gone = keyValue.Value;
+                    Guard.Remove(keyValue.Key);
+                }
+            }
+
+            Console.WriteLine("\nПромежутки времени, в течение которых галерея остается без охраны:");
+            if (Protected.Count > 0)
+            {
+                foreach (KeyValuePair<double, double> keyValue in Protected)
+                    Console.WriteLine("{0:0.00} - {1:0.00}", keyValue.Key, keyValue.Value);
+            }    
+            else
+                Console.WriteLine("Охраняется всегда");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab 9/Lab 9 Problem solving (1.3)/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Collections.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab_9_Problem_solving__1._3_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество задач (1 <= N <= 100) \nN = ");
+            int N = int.Parse(Console.ReadLine());
+            Console.Write("Введите исходное умение (1 <= A <= 100) \nA = ");
+            int A = int.Parse(Console.ReadLine());
+
+            MultiValueDictionary<int, int> Task_Skill = new MultiValueDictionary<int, int>(N);
+            Console.WriteLine("Введите сколько умения нужно для решения задачи и сколько умения прибавится после ее решения (через пробел)");
+            for (int i = 0; i < N; i++)
+            {
+                string temp = Console.ReadLine();
+                Task_Skill.Add(int.Parse(temp.Split(" ")[0]), int.Parse(temp.Split(" ")[1]));
+            }
+
+            int count = 0;
+            foreach (KeyValuePair<int, IReadOnlyCollection<int>> key in Task_Skill.OrderBy(key => key.Key))
+            {
+                if (A >= key.Key)
+                {
+                    foreach (int value in key.Value)
+                    {
+                        A += value;
+                        count++;
+                    }
+                }
+                else
+                    break;
+            }
+
+            Console.WriteLine("Максимальное количество задач, которое можно решить = {0}", count);
+            Console.ReadKey();
+        }
+    }
+}
